2eb896b2fbda31404e92d302d7557ea4b7b0783b
demoppal <burmesearchive85@gmail.com>
2026-05-01 21:03:01 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
-import aa
+import xx
+import asyncio
+import sys
+
+async def start():
+    try:
+        # xx.so ထဲက main_menu ကို async စနစ်နဲ့ ခေါ်တာပါ
+        await xx.main_menu()
+    except Exception as e:
+        print(f"Error: {e}")
+
 if __name__ == "__main__":
-    aa.main()
-  
+    try:
+        # Async function ကို run ပေးတဲ့ command ပါ
+        asyncio.run(start())
+    except KeyboardInterrupt:
+        pass
+
